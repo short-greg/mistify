6d610cdc0de2ee24ccb5acf48ab81cc43a2e9da9
Short <gshort23@yahoo.com>
2023-02-19 12:26:10 +0900

    add test for clamping

--- mistify/tests/test_fuzzy.py
+++ mistify/tests/test_fuzzy.py
@@ -114,6 +114,12 @@ class TestMaxMin(object):
         result = composition.forward(fuzzy_set)
         assert result.data.size() == torch.Size([4, 2, 4])
 
+    def test_clamp_clamps_weights_between_zero_and_one(self):
+        maxmin = fuzzy.MaxMin(4, 2)
+        maxmin.weight.data.data = torch.randn(maxmin.weight.data.size())
+        maxmin.clamp()
+        assert ((maxmin.weight.data.data >= 0.0) & (maxmin.weight.data.data <= 1.0)).all()
+
 class TestMaxProd(object):
     
     def test_forward_outputs_correct_size_with_no_variables(self):
