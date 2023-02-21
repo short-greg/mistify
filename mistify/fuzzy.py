fix bugs in numerical underflow for adamax

mistify/fuzzy.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 from .utils import reduce, get_comp_weight_size
 from abc import abstractmethod
-from .base import CompositionBase, ComplementBase, maxmin, minmax, maxprod
+from .core import CompositionBase, ComplementBase, maxmin, minmax, maxprod
 import math
 from functools import partial
 

mistify/ternary.py
@@ -2,7 +2,7 @@ import typing
 import torch
 import torch.nn as nn
 import typing
-from .base import CompositionBase, maxmin, ComplementBase
+from .core import CompositionBase, maxmin, ComplementBase
 from .utils import get_comp_weight_size
 
 # Add in TernarySet as a type of crisp set