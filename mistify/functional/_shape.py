d26c0b2af9fed9e580e68696a1390e814a7d30ba
Short <gshort23@yahoo.com>
2024-03-17 14:13:53 +0900

    add tests for triangle and trapezoid

--- mistify/functional/_shape.py
+++ mistify/functional/_shape.py
@@ -14,9 +14,10 @@ def _triangle_pre_hook(grad, x, left, right, clip, state):
     out_grad = state['grad']
     oob = (x < left) | (x > right)
     grad = grad.clone()
-
+    print(grad, oob)
     if clip is not None:
         grad[oob] = out_grad[oob].clamp(-clip, clip)
+        print(grad[oob])
     else:
         grad[oob] = out_grad[oob]
     return grad
@@ -29,8 +30,10 @@ def triangle(
 ) -> torch.Tensor:
 
     state = {}
+    x_base = x
+    x = x_base.clone()
     if clip != 0.0 and g is True:
-        x.register_hook(partial(_triangle_pre_hook, x=x, left=left, right=right, state=state, clip=clip))
+        x.register_hook(partial(_triangle_pre_hook, x=x_base, left=left, right=right, state=state, clip=clip))
 
     left_val = height / (mid - left) * (x - left)
     right_val = -height / (right - mid) * (x - mid) + height
@@ -38,7 +41,7 @@ def triangle(
     right_side = x >= mid
     left_val[right_side] = right_val[right_side]
     if clip != 0.0 and g is True:
-        left_val.register_hook(partial(_triangle_post_hook, x=x, state=state))
+        left_val.register_hook(partial(_triangle_post_hook, state=state))
     return left_val
 
 
@@ -77,8 +80,15 @@ def trapezoid(
 ) -> torch.Tensor:
 
     state = {}
+    x_base = x
+    x = x_base.clone()
     if clip != 0.0 and g is True:
-        x.register_hook(partial(_trap_pre_hook, x=x, left=left, mid1=mid1, mid2=mid2, right=right, state=state))
+        x.register_hook(
+            partial(_trap_pre_hook, 
+                    x=x_base, left=left,
+                    mid1=mid1, mid2=mid2, 
+                    right=right, state=state, 
+                    clip=clip))
     left_val = height / (mid1 - left) * (x - left)
     right_val = -height / (right - mid2) * (x - mid2) + height
     
@@ -89,19 +99,20 @@ def trapezoid(
     y = left_val
 
     if clip != 0.0 and g is True:
-        y.register_hook(partial(_trap_post_hook, x=x, state=state))
+        y.register_hook(partial(_trap_post_hook, state=state))
     return y
 
 
 def isosceles_trapezoid(
     x: torch.Tensor, left: TENSOR_FLOAT, mid1: TENSOR_FLOAT, 
     mid2: TENSOR_FLOAT, height: TENSOR_FLOAT=1., 
-    grad_clip: float=0.0
+    g: bool=False,
+    clip: float=0.0
 ) -> torch.Tensor:
     
     dl = mid1 - left
     right = mid2 + dl
     return trapezoid(
-        x, left, mid1, mid2, right, height, grad_clip
+        x, left, mid1, mid2, right, height, g, clip
     )
 
---
+++ tests/functional/test_shape.py
@@ -0,0 +1,126 @@
+from mistify.functional import _shape as F
+import torch
+
+T = torch.tensor
+
+class TestTriangle:
+    
+    def test_triangle_outputs_correct_value(self):
+
+        x = T([0.5, 1.5])
+        y = F.triangle(x, T([0.0]), T([1.0]), T([2.0]))
+        assert (y == torch.tensor([0.5, 0.5])).all()
+
+    def test_triangle_outputs_correct_shape(self):
+
+        x = torch.rand(4, 2)
+        left = torch.rand(1, 2)
+        mid = left + torch.rand(1, 2)
+        right = mid + torch.rand(1, 2)
+        y = F.triangle(x, left, mid, right)
+        assert y.shape == torch.Size([4, 2])
+
+    def test_triangle_outputs_gradients_if_oob(self):
+
+        torch.manual_seed(2)
+        x = torch.rand(4, 2, requires_grad=True)
+        x.retain_grad()
+        left = torch.rand(1, 2)
+        mid = left + torch.rand(1, 2)
+        right = mid + torch.rand(1, 2)
+        y = F.triangle(x, left, mid, right, g=True, clip=0.1)
+        y.sum().backward()
+        assert (x.grad != 0.0).any()
+
+
+class TestIsosceles:
+
+    def test_isosceles_outputs_correct_value(self):
+
+        x = T([0.5, 1.5])
+        y = F.isosceles(x, T([0.0]), T([1.0]))
+        assert (y == torch.tensor([0.5, 0.5])).all()
+
+    def test_isosceles_outputs_correct_shape(self):
+
+        x = torch.rand(4, 2)
+        left = torch.rand(1, 2)
+        mid = left + torch.rand(1, 2)
+        y = F.isosceles(x, left, mid)
+        assert y.shape == torch.Size([4, 2])
+
+    def test_isosceles_outputs_gradients_if_oob(self):
+
+        torch.manual_seed(2)
+        x = torch.rand(4, 2, requires_grad=True)
+        x.retain_grad()
+        left = torch.rand(1, 2)
+        mid = left + torch.rand(1, 2)
+        y = F.isosceles(x, left, mid, g=True, clip=0.1)
+        y.sum().backward()
+        assert (x.grad != 0.0).any()
+
+
+
+class TestTrapezoid:
+    
+    def test_trapezoid_outputs_correct_value(self):
+
+        x = T([0.5, 1.5, 2.5])
+        y = F.trapezoid(x, T([0.0]), T([1.0]), T([2.0]), T([3.0]))
+        assert (y == torch.tensor([0.5, 1.0, 0.5])).all()
+
+    def test_trapezoid_outputs_correct_shape(self):
+
+        x = torch.rand(4, 2)
+        left = torch.rand(1, 2)
+        mid = left + torch.rand(1, 2)
+        mid2 = mid + torch.rand(1, 2)
+        right = mid2 + torch.rand(1, 2)
+        y = F.trapezoid(x, left, mid, mid2, right)
+        assert y.shape == torch.Size([4, 2])
+
+    def test_trapezoid_outputs_gradients_if_oob(self):
+
+        torch.manual_seed(2)
+        x = torch.rand(4, 2, requires_grad=True)
+        x.retain_grad()
+        left = torch.rand(1, 2)
+        mid = left + torch.rand(1, 2)
+        mid2 = mid + torch.rand(1, 2)
+        right = mid2 + torch.rand(1, 2)
+        y = F.trapezoid(x, left, mid, mid2, right, g=True, clip=0.1)
+        y.sum().backward()
+        assert (x.grad != 0.0).any()
+
+
+
+class TestIsoscelesTrapezoid:
+
+    def test_isosceles_trapezoid_outputs_correct_value(self):
+
+        x = T([0.5, 1.5, 2.5])
+        y = F.isosceles_trapezoid(x, T([0.0]), T([1.0]), T([2.0]))
+        assert (y == torch.tensor([0.5, 1.0, 0.5])).all()
+
+    def test_isoscele_trapezoid_outputs_correct_shape(self):
+
+        x = torch.rand(4, 2)
+        left = torch.rand(1, 2)
+        mid = left + torch.rand(1, 2)
+        mid2 = mid + torch.rand(1, 2)
+        y = F.isosceles_trapezoid(x, left, mid, mid2)
+        assert y.shape == torch.Size([4, 2])
+
+    def test_isosceles_trapezoid_outputs_gradients_if_oob(self):
+
+        torch.manual_seed(2)
+        x = torch.rand(4, 2, requires_grad=True)
+        x.retain_grad()
+        left = torch.rand(1, 2)
+        mid = left + torch.rand(1, 2)
+        mid2 = mid + torch.rand(1, 2)
+        y = F.isosceles_trapezoid(x, left, mid, mid2, g=True, clip=0.1)
+        y.sum().backward()
+        assert (x.grad != 0.0).any()
+
