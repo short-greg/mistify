f789042dfd2a5da53a7e28adcec51310cc88a62d
Short <gshort23@yahoo.com>
2024-04-03 09:08:40 +0900

    add documentation to Gaussian

--- mistify/fuzzify/_shapes/_gaussian.py
+++ mistify/fuzzify/_shapes/_gaussian.py
@@ -17,6 +17,165 @@ import torch.nn.functional
 # Try to get simpler functions
 
 
+def gaussian_area(scale: torch.Tensor) -> torch.Tensor:
+    """
+    Args:
+        scale (torch.Tensor): The scale paramter (deviation) for the Gaussian
+
+    Returns:
+        torch.Tensor: the area of the Gaussian
+    """
+    return math.sqrt(2 * torch.pi) * scale
+
+
+def gaussian_invert(y: torch.Tensor, bias: torch.Tensor, scale: torch.Tensor) -> typing.Tuple[torch.Tensor, torch.Tensor]:
+    """Invert the Gaussian
+
+    Args:
+        y (torch.Tensor): The output of the Gaussian
+        bias (torch.Tensor): The bias of the Gaussian
+        scale (torch.Tensor): The scale (deviation) of the Gaussian
+
+    Returns:
+        typing.Tuple[torch.Tensor, torch.Tensor]: The two possible inverses for the Gaussian (lhs and rhs)
+    """
+    base = torch.sqrt(-2 * scale ** 2 * torch.log(y + 1e-7))
+    return bias - base, bias + base
+
+
+def gaussian_area_up_to(x: torch.Tensor, bias: torch.Tensor, scale: torch.Tensor) -> torch.Tensor:
+    """Calculate the area up to a value
+
+    Args:
+        x (torch.Tensor): The value to calculate the area up to
+        bias (torch.Tensor): The bias of the Gaussian
+        scale (torch.Tensor): The scale (deviation) of the Gaussian
+
+    Returns:
+        torch.Tensor: The area
+    """
+    return math.sqrt(math.pi / 2) * (
+        1 / torch.sqrt(scale ** -2) - scale * torch.erf(
+            (bias - x) / (math.sqrt(2) * scale)
+        )
+    )
+    # return (0.5 + 0.5 * torch.erf((x - bias) / (math.sqrt(2) * scale))) * math.sqrt(2 * math.pi) * scale
+    # return scale * math.sqrt(torch.pi) / math.sqrt(2.0) * torch.erf(
+    #     (x - bias) / (math.sqrt(2.) * scale)
+    # ) + torch.erf(bias / (scale * math.sqrt(2.0)))
+
+
+def gaussian_area_up_to_inv(area: torch.Tensor, bias: torch.Tensor, scale: torch.Tensor, increasing: bool=True) -> torch.Tensor:
+    """Invert the area function of the Gaussian
+
+    Args:
+        area (torch.Tensor): The area to invert
+        bias (torch.Tensor): The bias of the Gaussian
+        scale (torch.Tensor): The scale (deviation) of the Gaussian 
+        increasing (bool, optional): Whether it is increasing or decreasing. Defaults to True.
+
+    Returns:
+        torch.Tensor: The x value
+    """
+    # area = m
+    # c = scale
+    # b = bias
+    # a = weight
+    
+    # left = math.sqrt(2.0) * area / (scale * math.sqrt(math.pi))
+    # right = torch.erf(
+    #     bias / (scale * math.sqrt(2.))
+    # )
+    left = -area * math.sqrt(2 * math.pi)
+    right = math.pi / torch.sqrt(scale ** -2)
+    dx = math.sqrt(2.) * scale * torch.erfinv(
+        (left + right) / (math.pi * scale)
+    )
+    # dx = scale * math.sqrt(2.) * torch.erfinv(left - right)
+    return bias - dx if increasing else bias + dx
+
+
+def gaussian(x: torch.Tensor, bias: torch.Tensor, scale: torch.Tensor) -> torch.Tensor:
+    """The Gaussian function
+
+    Args:
+        x (torch.Tensor): The input
+        bias (torch.Tensor): The bias of the Gaussian
+        scale (torch.Tensor): The scale (deviation) of the Gaussian
+
+    Returns:
+        torch.Tensor: The output of the Gaussian
+    """
+    return torch.exp(-((x - bias) ** 2) / (2 * (scale **2)))
+
+
+def truncated_gaussian_area(bias: torch.Tensor, std: torch.Tensor, height: torch.Tensor) -> torch.Tensor:
+    """
+
+    Args:
+        bias (torch.Tensor): The bias of the Gaussian
+        std (torch.Tensor): The deviation of the Gaussian
+        height (torch.Tensor): The height of the truncation
+
+    Returns:
+        torch.Tensor: The area of the truncated Gaussian
+    """
+    pts = gaussian_invert(height, bias, std)
+    rec_area = (pts[1] - pts[0]) * height
+    gauss_area = gaussian_area_up_to(pts[0], bias, std)
+    return rec_area + 2 * gauss_area
+
+
+def truncated_gaussian_mean_core(bias: torch.Tensor, scale: torch.Tensor, height: torch.Tensor) -> torch.Tensor:
+    """
+    Args:
+        bias (torch.Tensor): The bias of the Gaussian
+        scale (torch.Tensor): The scale (deviation) of the Gaussian
+        height (torch.Tensor): The height of the Gaussian
+
+    Returns:
+        torch.Tensor: The mean of the core of the Gaussian
+    """
+    pts = gaussian_invert(height, bias, scale)
+    return (pts[0] + pts[1]) / 2.0
+
+
+def half_gaussian_area(scale: torch.Tensor) -> torch.Tensor:
+    # pts = gaussian_invert(height, bias, std)
+    # rec_area = (bias - pts[0]) * height
+    # gauss_area = gaussian_area_up_to(pts[0], bias, std)
+    return gaussian_area(scale) / 2.0
+
+
+def half_gaussian_centroid(bias: torch.Tensor, std: torch.Tensor, height: torch.Tensor, increasing: bool=True) -> torch.Tensor:
+
+    return gaussian_area_up_to_inv(height, bias, std, increasing)
+
+
+def truncated_half_gaussian_area(bias: torch.Tensor, std: torch.Tensor, height: torch.Tensor) -> torch.Tensor:
+    pts = gaussian_invert(height, bias, std)
+    rec_area = (bias - pts[0]) * height
+    gauss_area = gaussian_area_up_to(pts[0], bias, std)
+    return gauss_area + rec_area
+
+
+def truncated_half_gaussian_mean_core(bias: torch.Tensor, std: torch.Tensor, height: torch.Tensor, increasing: bool=True) -> torch.Tensor:    
+    pts = gaussian_invert(height, bias, std)
+    if increasing:
+        return pts[0]
+    return pts[1]
+
+
+def truncated_half_gaussian_centroid(bias: torch.Tensor, std: torch.Tensor, height: torch.Tensor, increasing: bool=True) -> torch.Tensor:
+    pts = gaussian_invert(height, bias, std)
+    rec_area = (bias - pts[0]) * height
+    gauss_area = gaussian_area_up_to(pts[1], bias, std)
+    gauss_centroid = gaussian_area_up_to_inv(gauss_area / 2.0, bias, std, increasing)
+    rec_centroid = (bias + pts[0]) / 2.0 if increasing else (bias + pts[1]) / 2.0
+
+    return (rec_centroid * rec_area + gauss_centroid * gauss_area) / (rec_area + gauss_area)
+
+
 class Gaussian(Nonmonotonic):
 
     def __init__(
@@ -71,108 +230,23 @@ class Gaussian(Nonmonotonic):
         )
 
 
-def gaussian_area(scale):
-    
-    return math.sqrt(2 * torch.pi) * scale
-
-
-def gaussian_invert(y, bias, scale):
-    
-    base = torch.sqrt(-2 * scale ** 2 * torch.log(y + 1e-7))
-    return bias - base, bias + base
-
-
-def gaussian_area_up_to(x, bias, scale):
-    
-    return math.sqrt(math.pi / 2) * (
-        1 / torch.sqrt(scale ** -2) - scale * torch.erf(
-            (bias - x) / (math.sqrt(2) * scale)
-        )
-    )
-    # return (0.5 + 0.5 * torch.erf((x - bias) / (math.sqrt(2) * scale))) * math.sqrt(2 * math.pi) * scale
-    # return scale * math.sqrt(torch.pi) / math.sqrt(2.0) * torch.erf(
-    #     (x - bias) / (math.sqrt(2.) * scale)
-    # ) + torch.erf(bias / (scale * math.sqrt(2.0)))
-
-
-def gaussian_area_up_to_inv(area, bias, scale, increasing: bool=True):
-    # area = m
-    # c = scale
-    # b = bias
-    # a = weight
-    
-    # left = math.sqrt(2.0) * area / (scale * math.sqrt(math.pi))
-    # right = torch.erf(
-    #     bias / (scale * math.sqrt(2.))
-    # )
-    left = -area * math.sqrt(2 * math.pi)
-    right = math.pi / torch.sqrt(scale ** -2)
-    dx = math.sqrt(2.) * scale * torch.erfinv(
-        (left + right) / (math.pi * scale)
-    )
-    # dx = scale * math.sqrt(2.) * torch.erfinv(left - right)
-    return bias - dx if increasing else bias + dx
-
-
-def gaussian(x: torch.Tensor, bias: torch.Tensor, scale: torch.Tensor):
-    
-    return torch.exp(-((x - bias) ** 2) / (2 * (scale **2)))
-
-
-def truncated_gaussian_area(bias: torch.Tensor, std: torch.Tensor, height: torch.Tensor) -> torch.Tensor:
-    
-    pts = gaussian_invert(height, bias, std)
-    rec_area = (pts[1] - pts[0]) * height
-    gauss_area = gaussian_area_up_to(pts[0], bias, std)
-    return rec_area + 2 * gauss_area
-
-
-def truncated_gaussian_mean_core(bias: torch.Tensor, std: torch.Tensor, height: torch.Tensor) -> torch.Tensor:
-    pts = gaussian_invert(height, bias, std)
-    return (pts[0] + pts[1]) / 2.0
-
-
-def half_gaussian_area(scale: torch.Tensor) -> torch.Tensor:
-    # pts = gaussian_invert(height, bias, std)
-    # rec_area = (bias - pts[0]) * height
-    # gauss_area = gaussian_area_up_to(pts[0], bias, std)
-    return gaussian_area(scale) / 2.0
-
-
-def half_gaussian_centroid(bias: torch.Tensor, std: torch.Tensor, height: torch.Tensor, increasing: bool=True) -> torch.Tensor:
-
-    return gaussian_area_up_to_inv(height, bias, std, increasing)
-
-
-def truncated_half_gaussian_area(bias: torch.Tensor, std: torch.Tensor, height: torch.Tensor) -> torch.Tensor:
-    pts = gaussian_invert(height, bias, std)
-    rec_area = (bias - pts[0]) * height
-    gauss_area = gaussian_area_up_to(pts[0], bias, std)
-    return gauss_area + rec_area
-
-
-def truncated_half_gaussian_mean_core(bias: torch.Tensor, std: torch.Tensor, height: torch.Tensor, increasing: bool=True) -> torch.Tensor:    
-    pts = gaussian_invert(height, bias, std)
-    if increasing:
-        return pts[0]
-    return pts[1]
-
-
-def truncated_half_gaussian_centroid(bias: torch.Tensor, std: torch.Tensor, height: torch.Tensor, increasing: bool=True) -> torch.Tensor:
-    pts = gaussian_invert(height, bias, std)
-    rec_area = (bias - pts[0]) * height
-    gauss_area = gaussian_area_up_to(pts[1], bias, std)
-    gauss_centroid = gaussian_area_up_to_inv(gauss_area / 2.0, bias, std, increasing)
-    rec_centroid = (bias + pts[0]) / 2.0 if increasing else (bias + pts[1]) / 2.0
-
-    return (rec_centroid * rec_area + gauss_centroid * gauss_area) / (rec_area + gauss_area)
-
-
 class GaussianBell(Gaussian):
     """Use the GaussianBell function as the membership function
     """
+
+    def __init__(self, biases: ShapeParams, scales: ShapeParams, hookf=None):
+        """Use a Gaussian as the membership function
+
+        Args:
+            biases (ShapeParams): The biases for the Gaussian
+            scales (ShapeParams): The scale parameter for the Gaussian (deviation)
+            hookf (_type_, optional): The GradHook for the join function. Defaults to None.
+        """
+        super().__init__(biases, scales)
+        self.joinf = gaussian if hookf is None else hookf.f(gaussian)
+
     def join(self, x: Tensor) -> Tensor:
-        return gaussian(
+        return self.joinf(
             unsqueeze(x), self._biases.pt(0), self.sigma
         )
     
